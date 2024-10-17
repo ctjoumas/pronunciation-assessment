6dc1b1dbbbb3acf4526eb532ca1c1617e8565b62
William Xifaras <wxifaras@outlook.com>
2024-10-17 14:03:24 -0400

    testing

--- function_app.py
+++ function_app.py
@@ -1,5 +1,7 @@
 import azure.functions as func # type: ignore
 import logging
+import os
+import azure.cognitiveservices.speech as speechsdk
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -7,9 +9,42 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    ai_service_region = os.environ.get('AI_SERVICE_REGION')
+    ai_service_key = os.environ.get('AI_SERVICE_KEY')
+
     name = req.params.get('name')
     if not name:
         try:
+
+            speech_config = speechsdk.SpeechConfig(subscription=ai_service_key, region=ai_service_region)
+            audio_config = speechsdk.audio.AudioConfig(filename="")
+            speech_recognizer = speechsdk.SpeechRecognizer(
+            speech_config=speech_config, \
+            audio_config=audio_config)
+
+            topic = "the season of the fall"
+            pronunciation_config = speechsdk.PronunciationAssessmentConfig(
+                grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark,
+                granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme,
+                enable_miscue=False)
+            
+            pronunciation_config.enable_prosody_assessment()
+            pronunciation_config.enable_content_assessment_with_topic(topic)
+
+            # Create a speech recognizer using a file as audio input.
+            language = 'en-US'
+            speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language=language, audio_config=audio_config)
+            
+            # Apply pronunciation assessment config to speech recognizer
+            pronunciation_config.apply_to(speech_recognizer)
+
+            speech_recognition_result = speech_recognizer.recognize_once()
+            # The pronunciation assessment result as a Speech SDK object
+            pronunciation_assessment_result = speechsdk.PronunciationAssessmentResult(speech_recognition_result)
+
+            # The pronunciation assessment result as a JSON string
+            pronunciation_assessment_result_json = speech_recognition_result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
+
             req_body = req.get_json()
         except ValueError:
             pass
